ff141672dd956ce762000a1ab813364a0d2135a3
thelayermonkey <thelayermonkey@gmail.com>
2026-05-05 18:10:25 +0200

    Create photos.js

---
+++ api/photos.js
@@ -0,0 +1,17 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default async function handler(req, res) {
+  const result = await cloudinary.search
+    .expression('folder:dev/test001')
+    .sort_by('created_at', 'desc')
+    .max_results(50)
+    .execute();
+
+  res.status(200).json(result.resources);
+}
